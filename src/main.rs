08b7aa164ce94abbfe0ae6f9f31a3e95ffbe7bcc
Parker McMullin <parker.mcmullin01@gmail.com>
2022-04-15 11:23:09 -0700

    Add appropriation.

--- src/main.rs
+++ src/main.rs
@@ -42,6 +42,8 @@ fn App(cx: Scope) -> Element {
                 Ring { label: "Month".into(),  percent: percentages.1, radius: 100.0, color: RingColor::Yellow }
                 Ring { label: "Year".into(),   percent: percentages.0, radius: 60.0,  color: RingColor::Red }
             }
+
+            Appropriation {}
         }
     })
 }
@@ -162,6 +164,41 @@ fn Ring(
                     "{label}"
                 }
             }
+        }
+    })
+}
+
+#[inline_props]
+fn Appropriation(cx: Scope) -> Element {
+    cx.render(rsx! {
+        div {
+            class: "group fixed bottom-10 right-0 left-2",
+            div {
+                class: "transition-opacity group-hover:opacity-0 absolute right-6",
+                Heart {}
+            }
+            div {
+                class: "transition-opacity opacity-0 group-hover:opacity-100 absolute right-2 xs:right-6 text-center text-xs text-gray-700 dark:text-gray-400",
+                "Made with "
+                Heart {}
+                " by Parker McMullin (aka. "
+                a {
+                    href: "https://twitter.com/parker_codes",
+                    target: "_blank",
+                    "@parker_codes"
+                }
+                ")"
+            }
+        }
+    })
+}
+
+#[inline_props]
+fn Heart(cx: Scope) -> Element {
+    cx.render(rsx! {
+        span {
+            class: "text-red-500 dark:text-red-700",
+            "❤"
         }
     })
 }
